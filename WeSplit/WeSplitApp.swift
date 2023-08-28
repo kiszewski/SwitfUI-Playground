a5c6131caa19b4c8f80e6f1099534300e9531a40
Leonardo Kiszewski <kiszewski1999@gmail.com>
2023-08-28 19:30:18 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Leonardo Kiszewski on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
